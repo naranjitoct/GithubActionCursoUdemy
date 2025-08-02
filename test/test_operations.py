1e6f0ff914801944aa81f3384b60711b73321e99
Ric SG <58696823+naranjitoct@users.noreply.github.com>
2025-08-02 12:32:28 +0200

    otra más...

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,10 +1,10 @@
 from src.math_operations import add,sub
 
-def test_add (a,b):
+def test_add ():
     assert add (2,3)==5
     assert add (4,3)==7
 
-def test_sub(a,b):
+def test_sub():
     assert sub(5,3)==2
     assert sub(8,3)==5
     assert sub(5,1)==4
